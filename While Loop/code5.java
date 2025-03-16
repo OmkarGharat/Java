aff917cc70eb8e57f962d0d1c814924665b8b43d
Omkar Gharat <83211956+OmkarGharat@users.noreply.github.com>
2025-03-16 21:50:39 +0530

    Create code5.java

---
+++ While Loop/code5.java
@@ -0,0 +1,12 @@
+public class Project3 {
+public static void main(String[] args) {
+		int i = 7;
+		while (i <= 50) {
+			if (i == 35) {
+				break;
+			}
+			System.out.println(i);
+			i += 7;
+		}
+	}
+}
